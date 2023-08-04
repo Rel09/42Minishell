a97912c0a659d2db3633e9b98b1fa900b8b80f87
dpotvin <davkojak@hotmail.com>
2023-08-04 05:42:05 -0400

    added ft_longlongatoi

--- srcs/tools/ft_atoi.c
+++ srcs/tools/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dpotvin <dpotvin@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 21:10:43 by pbergero          #+#    #+#             */
-/*   Updated: 2023/08/04 05:33:32 by dpotvin          ###   ########.fr       */
+/*   Updated: 2023/08/04 05:41:51 by dpotvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,33 +47,31 @@ int	ft_atoi(const char *nptr)
 	return (nbr * sign);
 }
 
-long long ft_longlongatoi(const char *nptr)
+long long	ft_longlongatoi(const char *nptr)
 {
-    int	i;
-	int sign;
-	long long nbr;
-	
+	int			i;
+	int			sign;
+	long long	nbr;
+
 	if (!nptr)
-        return 0;
+		return (0);
 	if (!ft_strcmp(nptr, "−9223372036854775808"))
 		return (LLONG_MIN);
-    i = 0;
-    nbr = 0;
-    sign = 1;
-    while (ft_isspace(nptr))
-        i++;
-    if (nptr[i] == '-' || nptr[i] == '+')
-    {
-        if (nptr[i] == '-')
-            sign = -1;
-        i++;
-    }
-    while (nptr[i] >= '0' && nptr[i] <= '9')
-    {
-        if (nbr > (LLONG_MAX - (nptr[i] - '0')) / 10)
-            return 0;
-        nbr = (nbr * 10) + (nptr[i] - '0');
-        i++;
-    }
-    return (nbr * sign);
+	i = 0;
+	nbr = 0;
+	sign = 1;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i++] == '-')
+			sign = -1;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		if (nbr > (LLONG_MAX - (nptr[i] - '0')) / 10)
+			return (0);
+		nbr = (nbr * 10) + (nptr[i++] - '0');
+	}
+	return (nbr * sign);
 }
