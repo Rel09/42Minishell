c8ac322db27aa31b2379704d1305f9c2c4cfa10f
D <p@mac.local>
2023-08-10 01:54:45 -0400

    fixed prompt on signal

--- srcs/execution/signal.c
+++ srcs/execution/signal.c
@@ -23,9 +23,10 @@ void	sigint_running_shell(int sig)
 void	sigint_interactive(int sig)
 {
 	(void) sig;
+	ft_putstr_fd("\n", STDOUT_FILENO);
 	rl_on_new_line();
-	ft_putstr_fd("\n", 1);
 	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 int	intercept_signals(void)
